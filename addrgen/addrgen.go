187ae5461c77e9ba4562feabb7da4847ccc78880
Vladislav Yarmak <vladislav-ex-src@vm-0.com>
2023-10-21 19:25:43 +0300

    WIP

--- addrgen/addrgen.go
+++ addrgen/addrgen.go
@@ -1,7 +1,10 @@
 package addrgen
 
 import (
+	"errors"
+	"fmt"
 	"math/big"
+	"strings"
 )
 
 type AddrGetter interface {
@@ -9,8 +12,39 @@ type AddrGetter interface {
 	Power() *big.Int
 }
 
+type PortGetter interface {
+	Port() uint16
+	Power() uint16
+}
+
 type AddrSet struct {
-	portBase   uint16
-	portNum    uint16
+	portRange  PortGetter
 	addrRanges []AddrGetter
+}
+
+func ParseAddrSet(spec string) (*AddrSet, error) {
+	lastColonIdx := strings.LastIndex(spec, ":")
+	if lastColonIdx == -1 {
+		return nil, errors.New("port specification not found - colon is missing")
+	}
+	addrPart := spec[:lastColonIdx]
+	portPart := spec[lastColonIdx+1:]
+	portRange, err := ParsePortRangeSpec(portPart)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse port part: %w", err)
+	}
+
+	terms := strings.Split(addrPart, ",")
+	addrRanges := make([]AddrGetter, 0, len(terms))
+	for _, addrRangeSpec := range terms {
+		r, err := ParseAddrRangeSpec(addrRangeSpec)
+		if err != nil {
+			return nil, fmt.Errorf("addr range spec %q parse failed: %w", addrRangeSpec, err)
+		}
+		addrRanges = append(addrRanges, r)
+	}
+	return &AddrSet{
+		portRange: portRange,
+		addrRanges: addrRanges,
+	}, nil
 }
--- addrgen/range.go
+++ addrgen/range.go
@@ -2,9 +2,11 @@ package addrgen
 
 import (
 	"errors"
+	"fmt"
 	"math/big"
 	"math/rand"
 	"net/netip"
+	"strings"
 
 	"github.com/Snawoot/dtlspipe/randpool"
 )
@@ -41,6 +43,24 @@ func NewAddrRange(start, end netip.Addr) (*AddrRange, error) {
 	}, nil
 }
 
+func NewAddrRangeFromPrefix(pfx netip.Prefix) (*AddrRange, error) {
+	if !pfx.IsValid() {
+		return nil, errors.New("invalid prefix")
+	}
+	pfx = pfx.Masked()
+	addr := pfx.Addr()
+	base := new(big.Int)
+	base.SetBytes(addr.AsSlice())
+	pfxPower := addr.BitLen() - pfx.Bits()
+	size := big.NewInt(1)
+	size.Lsh(size, uint(pfxPower))
+	return &AddrRange{
+		base: base,
+		size: size,
+		v6:   addr.BitLen() == 128,
+	}, nil
+}
+
 func (ar *AddrRange) Addr() string {
 	res := new(big.Int)
 	randpool.Borrow(func(r *rand.Rand) {
@@ -64,4 +84,38 @@ func (ar *AddrRange) Power() *big.Int {
 	res := new(big.Int)
 	res.Set(ar.size)
 	return res
+}
+
+func ParseAddrRangeSpec(spec string) (AddrGetter, error) {
+	switch {
+	case strings.Contains(spec, "/"):
+		pfx, err := netip.ParsePrefix(spec)
+		if err != nil {
+			return nil, fmt.Errorf("unable to parse prefix %q: %w", spec, err)
+		}
+		if pfx.IsSingleIP() {
+			return SingleAddr(pfx.Addr().String()), nil
+		}
+		r, err := NewAddrRangeFromPrefix(pfx)
+		if err != nil {
+			return nil, fmt.Errorf("unable to parse range spec %q: %w", spec, err)
+		}
+		return r, nil
+	case strings.Contains(spec, ".."):
+		parts := strings.SplitN(spec, "..", 2)
+		start, err := netip.ParseAddr(parts[0])
+		if err != nil {
+			return nil, fmt.Errorf("unable to parse addr %q: %w", parts[0], err)
+		}
+		end, err := netip.ParseAddr(parts[1])
+		if err != nil {
+			return nil, fmt.Errorf("unable to parse addr %q: %w", parts[1], err)
+		}
+		r, err := NewAddrRange(start, end)
+		if err != nil {
+			return nil, fmt.Errorf("invalid range spec %q: %w", spec, err)
+		}
+		return r, nil
+	}
+	return SingleAddr(spec), nil
 }
